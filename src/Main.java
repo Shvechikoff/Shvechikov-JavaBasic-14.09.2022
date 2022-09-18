60e495f36b69fdf249c8936be50fe22c579335d1
Illia <illia.shvechikov@gmail.com>
2022-09-18 20:45:11 +0300

    Renamed class to "Main"

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
-
-// сообщение -- сканер -- условие+сообщение -- допольнительные условия
 public class Main {
-
     public static void main(String[] args) {
-
-        System.out.println("Write random number from 1 to 6");
-
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-
-        if (number == 4) {
-            System.out.println("It`s right. You WIN! =)");
-        } else {
-            System.out.println("Better luck next time, its 4 =(");
-        }
-
+        int a = 12;
+        int b = 14;
+        int c = 14;
+// V = a (length) * b (width) * c (height)
+        int volume = a * b * c;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        int length = 4 * a + 4 * b + 4 * c;
+        System.out.println("Сума сторін паралелепіпеда = " + length);
     }
 }
